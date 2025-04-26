670d8c2ed46f0d92273a62d33ddbff3365b67f87
LucasHenrique569 <cs206838@gmail.com>
2025-04-25 22:08:04 -0400

    criando_estrutura_inicial

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+    colors: {
+        primary: '#0984E3',
+        secondary: '#FF872C',
+        background: '#F5F5F5',
+        error: '#B00200',
+        text: '#000000',
+    },
+    fonts: {
+        bold: 'Roboto-Bold',
+        medium: 'Roboto-medium',
+        regular: 'Roboto-regular',
+        light: 'Roboto-Light',
+        thin: 'Roboto-Thin',
+    },
+    spacing: {
+        small: 8,
+        medium: 16,
+        large: 24,
+        xlarge: 32,
+    },
+    borderRadius: {
+        small: 4,
+        medium: 8,
+        large: 16,
+        xlarge: 32,
+    }
+}
