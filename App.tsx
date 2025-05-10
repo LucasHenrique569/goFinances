770df34d47147c7643ff7aef942534355b5d1a48
LucasHenrique569 <cs206838@gmail.com>
2025-05-09 21:40:22 -0400

    finalizando_branch_componentes

--- App.tsx
+++ App.tsx
@@ -5,7 +5,27 @@ import { ThemeProvider } from 'styled-components';
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
 
+import AppLoading, { AppLoadingProps } from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_900Black,
+} from '@expo-google-fonts/poppins'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_900Black,
+  })
+
+  if (!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,33 @@
 import { Text } from "react-native";
 import { 
     Container, 
-    Title 
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
 } from "./styles";
+
+import { Feather } from '@expo/vector-icons'
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={ { uri: 'https://upload.wikimedia.org/wikipedia/pt/thumb/f/f5/Boston_Celtics.png/250px-Boston_Celtics.png' } }/>
+                        <User>
+                            <UserGreeting>Olá</UserGreeting>
+                            <UserName>Aluno</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Feather name="power"/>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
